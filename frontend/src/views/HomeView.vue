c32ad1f1a1a3dc4c2f2de476466913b7a486f138
Luis Abaroa <labaroasordo@gmail.com>
2024-07-18 20:18:26 -0600

    chore: Create EventList component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,13 @@
-<script setup></script>
+<script setup>
+import EventList from "../components/EventList.vue";
+</script>
 
 <template>
-  <div class="h-100">
-    <h1>Home</h1>
-    <p>Welcome to the home page</p>
+  <div class="h-100 d-flex flex-column p-3">
+    <div class="mt-5 mx-5">
+      <h1>Home</h1>
+      <h2>Savor the Flavor: A Culinary Adventure Awaits!</h2>
+      <EventList />
+    </div>
   </div>
 </template>

9abd228c27505fb39a8e9b3b9d5d294cde81bc88
Luis Abaroa <labaroasordo@gmail.com>
2024-08-07 18:56:36 -0600

    refactor: Add custom margin and padding utility classes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,7 @@ import router from "./router";
 
 // Import Bootstrap and BootstrapVue CSS files
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.min.js'
 import "bootstrap";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
 
 // Import FontAwesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -15,6 +12,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 // Import custom SCSS
 import "../scss/custom.scss";
 import '../scss/responsive.scss';
+import '../scss/anims.scss';
 
 // Import VCalendar and its styles
 import VCalendar from 'v-calendar';

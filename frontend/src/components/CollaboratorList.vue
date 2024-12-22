1281ec4abece2cc7f2c97b01c6a3264b44574dfd
Luis Abaroa <labaroasordo@gmail.com>
2024-12-22 11:55:00 -0600

    feat: Add CollaboratorList component and enhance HomeView layout

---
+++ frontend/src/components/CollaboratorList.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { capitalizeKebab } from "@/utils/functions";
+
+const props = defineProps({
+  title: String,
+  collaborators: Array,
+  gridLayout: Boolean,
+});
+</script>
+
+<template>
+  <div class="h-100 w-100">
+    <div class="d-flex flex-row align-items-center">
+      <h2 class="me-4">{{ title }}</h2>
+    </div>
+    <div v-if="collaborators?.length">
+      <div class="w-100">
+        <div :class="gridLayout ? 'row' : 'd-flex overflow-auto'">
+          <div
+            v-for="collaborator in collaborators"
+            :key="collaborator.id"
+            :class="
+              gridLayout
+                ? 'col-3 col-sm-6 col-md-4 col-lg-3 mb-4'
+                : 'flex-shrink-0 p-2 w-25-lg-w-50-sm'
+            "
+            class="text-center"
+          >
+            <img
+              :src="collaborator.image"
+              :alt="collaborator.name"
+              class="rounded-circle img-fluid mb-3 object-fit-cover"
+              style="width: 150px; height: 150px"
+            />
+            <h5 class="fw-bold mb-3">{{ collaborator.name }}</h5>
+            <div class="btn btn-primary">About Me</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { computed } from "vue";
 import EventList from "../components/EventList.vue";
-import img from "@/assets/images/chef.png";
-import ImageGrid from "@/components/ImageGrid.vue";
+import img from "@/assets/images/dinner.jpg";
 import SocialIcons from "@/components/SocialIcons.vue";
-import CardComponent from "@/components/CardComponent.vue";
-import ProductsCarousel from "@/components/ProductsCarousel.vue";
 import { useEventStore } from "@/stores/useEventStore";
 import { useCollaboratorStore } from "@/stores/useCollaboratorStore";
 import { useProductStore } from "@/stores/useProductStore";
+import strings from "@/utils/strings";
+import CollaboratorList from "../components/CollaboratorList.vue";
 
 const eventStore = useEventStore();
 const collaboratorStore = useCollaboratorStore();
@@ -34,94 +33,63 @@ const mixedImages = computed(() => {
 </script>
 
 <template>
-  <div class="h-100 d-flex flex-column p-3">
-    <div class="mt-5 mx-3">
+  <div class="d-flex flex-column overflow-auto">
+    <div class="position-relative" style="height: 60vh">
       <div
-        class="d-flex flex-row-lg-column-sm justify-content-center h-30 my-2"
-      >
-        <div
-          class="d-flex flex-row align-items-center justify-content-center w-50-lg-w-100-sm pe-1 mb-2"
-        >
-          <img
-            class="w-50 h-100 object-fit-cover"
-            style="filter: drop-shadow(5px 5px 10px black)"
-            :src="img"
-          />
-          <div class="ms-4">
-            <h1 class="fw-bold">JOGA</h1>
-            <h2 class="fs-4">Savor the Flavor: A Culinary Adventure Awaits!</h2>
+        class="position-absolute w-100 h-100"
+        :style="{
+          background: `url(${img})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          filter: 'opacity(0.4) blur(3px)',
+          zIndex: -1,
+        }"
+      ></div>
+      <div class="position-absolute top-50 start-0 translate-middle-y mx-6">
+        <h1 class="fw-bold">{{ strings.title.toUpperCase() }}</h1>
+        <span class="fs-6">{{ strings.homeSubtitle }} </span>
+      </div>
+    </div>
+
+    <div class="h-100 mx-5">
+      <CollaboratorList
+        class="mt-7"
+        title="CHEFS"
+        :gridLayout="false"
+        :collaborators="collaboratorStore.collaborators"
+      />
+
+      <hr class="mb-7" />
+
+      <EventList title="EVENTS" :events="eventStore.events" class="mb-7" />
+
+      <div class="container my-5">
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-4 g-4">
+          <div
+            v-for="pill in infoPills"
+            :key="`pill-${pill.title}`"
+            class="col d-flex justify-content-center position-relative hover-bounce"
+          >
+            <div
+              class="rounded-pill position-absolute w-80 h-100"
+              :style="{
+                backgroundImage: 'url(' + mixedImages?.[pill.id] + ')',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                filter: 'brightness(25%) grayscale(100%)',
+                zIndex: -1,
+              }"
+            ></div>
+            <span
+              class="badge rounded-pill w-100 d-flex flex-column align-items-center justify-content-center text-white"
+            >
+              <span class="fs-1">{{ pill.value }}</span>
+              <p class="mt-2 fs-6">{{ pill.title }}</p>
+            </span>
           </div>
         </div>
-        <div
-          class="d-flex align-items-center justify-content-center w-50-lg-w-100-sm h-100 mb-2"
-        >
-          <ImageGrid
-            :images="mixedImages"
-            style="filter: drop-shadow(16px 16px 16px black)"
-          />
-        </div>
       </div>
 
-      <CardComponent class="my-6 mx-3">
-        <div class="my-2 p-2">
-          <h2 class="text-center fw-bold">Who We Are</h2>
-          <p class="text-center fs-5">
-            We are a passionate team of food lovers dedicated to bringing
-            together the best culinary experiences from around the world. Our
-            mission is to create a platform where food enthusiasts can share,
-            learn, and enjoy the diverse flavors and techniques that make food a
-            universal language. Join us and be a part of this delicious journey!
-          </p>
-        </div>
-      </CardComponent>
-
-      <EventList title="Events" :events="eventStore.events" />
-
-      <CardComponent class="my-6 mx-3">
-        <div class="my-2 p-2">
-          <h2 class="text-center fw-bold">History</h2>
-          <p class="text-center fs-5">
-            Established in 2010, our food events planner has grown from a small
-            local gathering to a must-attend culinary festival. Each year,
-            thousands of food enthusiasts join us to celebrate the art of
-            cooking, tasting, and enjoying food. From gourmet chefs to home
-            cooks, everyone finds something to savor at our events.
-          </p>
-        </div>
-      </CardComponent>
-
-      <div v-if="collaboratorStore.collaborators" class="mt-5">
-        <ProductsCarousel
-          :images="collaboratorStore.collaborators.map((chef) => chef.image)"
-          :numberOfRows="1"
-          :withSpace="true"
-        />
-      </div>
-
-      <div class="d-flex flex-row justify-content-between w-100 my-5">
-        <div
-          v-for="pill in infoPills"
-          :key="`pill-${pill.title}`"
-          class="d-flex w-20 position-relative hover-bounce"
-        >
-          <div
-            class="rounded-pill position-absolute top-0 start-0 w-100 h-100"
-            :style="{
-              backgroundImage: 'url(' + mixedImages?.[pill.id] + ')',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              filter: 'brightness(25%) grayscale(100%)',
-              zIndex: -1,
-            }"
-          ></div>
-          <span
-            class="badge rounded-pill w-100 d-flex flex-column align-items-center justify-content-center text-white"
-          >
-            <span class="display-4">{{ pill.value }}</span>
-            <p class="mt-2 fs-5">{{ pill.title }}</p>
-          </span>
-        </div>
-      </div>
       <hr />
       <SocialIcons class="w-50" />
     </div>

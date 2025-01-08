e88ea853e995e2f695ea2dfb1ed008f25c16f941
Luis Abaroa <labaroasordo@gmail.com>
2025-01-07 19:40:28 -0600

    chore: Update main.yml env variables

---
+++ frontend/src/utils/strings.js
@@ -0,0 +1,9 @@
+const strings = {
+  title: "Joga",
+  homeSubtitle:
+    "Es una plataforma dedicada a impulsar a chefs emergentes y celebrar la gastronomía en todas sus formas. Nuestro espacio está diseñado para que puedas conocer a nuevos amigos o reencontrarte con aquellos que ya conoces. Aquí, cada comida se sirve al centro de la mesa, fomentando el compartir y celebrando la alegría de la sobremesa.",
+  instagram: import.meta.env.VITE_INSTAGRAM || "https://www.instagram.com",
+  whatsapp: import.meta.env.VITE_WHATSAPP || "https://wa.me",
+};
+
+export default strings;
